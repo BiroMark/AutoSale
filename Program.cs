3e5e63593b01cbcc500556005d0abfd601dcb15f
B_mark <birom@kkszki.hu>
2024-09-26 12:01:17 +0200

    Done

--- Program.cs
+++ Program.cs
@@ -28,6 +28,9 @@ namespace AutoSale
                 car.Id = dr.GetInt32(0);
                 car.Brand = dr.GetString(1);
                 car.Type = dr.GetString(2);
+                car.License = dr.GetString(3);
+                car.Date = dr.GetInt32(4);
+
                 cars.Add(car);
             } while (dr.Read());
 
@@ -39,11 +42,66 @@ namespace AutoSale
 
             conn.Connection.Close();
         }
+        public static void feladat2()
+        {
+            
+            string marka, tipus, azon;
+            int ev;
+            Console.WriteLine("Kérem az autó márkáját");
+            marka = Console.ReadLine();
+
+            Console.WriteLine("Kérem az autó tipusat");
+            tipus = Console.ReadLine();
+
+            Console.WriteLine("Kérem az autó motorkodjat");
+            azon = Console.ReadLine();
+
+            Console.WriteLine("Kérem az autó gyártási évét");
+            ev = Convert.ToInt32( Console.ReadLine());
+
+            string sql = $"INSERT INTO `cars`( `Brand`, `Type`, `License`, `Date`) VALUES ('{marka}','{tipus}','{azon}','{ev}')";
+            conn.Connection.Open();
+
+            MySqlCommand mySqlCommand = new MySqlCommand(sql, conn.Connection);
+            mySqlCommand.ExecuteNonQuery();
+            conn.Connection.Close() ;
+        }
+        public static void feladat3()
+        {
+            int ev, id;
+            Console.WriteLine("Kérem az auto idjet");
+            id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("kérem az új gyártási idejét");
+            ev = Convert.ToInt32( Console.ReadLine());
+            string sql = $"UPDATE `cars` SET `Date`='{ev}' WHERE `id`={id}";
+            conn.Connection.Open();
+            MySqlCommand mySqlCommand = new MySqlCommand(sql, conn.Connection);
+            mySqlCommand.ExecuteNonQuery();
+            conn.Connection.Close();
+        }
+        public static void feladat4()
+        {
+            int id;
+            Console.WriteLine("Kérem a törölni kívánt auto idjet");
+            id = Convert.ToInt32(Console.ReadLine());
+            string sql = $"DELETE FROM `cars` WHERE `id`={id}";
+            conn.Connection.Open();
+            MySqlCommand mySqlCommand = new MySqlCommand(sql, conn.Connection);
+            mySqlCommand.ExecuteNonQuery();
+            conn.Connection.Close();
+        }
         static void Main(string[] args)
         {
             feladat1();
-            Console.WriteLine(cars.Count);
-            Console.ReadLine();
+            foreach (var item in cars) 
+            {
+                Console.WriteLine($"Márka: {item.Brand}, azonositó: {item.License}");
+            }
+            feladat2();
+            feladat3();
+            feladat4();
+            
         }
     }
 }
