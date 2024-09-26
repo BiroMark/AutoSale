2e021414db4731d2da962513cc926163654d4806
B_mark <birom@kkszki.hu>
2024-09-26 10:15:38 +0200

    Ready to use

---
+++ Connect.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSale
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        string Host;
+        string Database;
+        string Username;
+        string Password;
+        string ConnectionString;
+
+        public Connect()
+        {
+            Host = "127.0.0.1";
+            Database = "Auto";
+            Username = "root";
+            Password = "";
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + Username + ";PASSWORD=" + Password + ";SslMode=None";
+            Connection = new MySqlConnection(ConnectionString);
+        } 
+
+    }
+}
