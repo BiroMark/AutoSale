b4a093d0401303de9c66ea44b296a8583fd76224
B_mark <birom@kkszki.hu>
2024-09-26 11:02:30 +0200

    localdat is available

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoSale.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +9,41 @@ using System.Threading.Tasks;
 
 namespace AutoSale
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static Connect conn = new Connect();
+        public static List<Car> cars = new List<Car>();
+        public static void feladat1() 
         {
             
+            string sql = "SELECT * FROM cars";
+
+            conn.Connection.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            do
+            {
+                Car car = new Car();
+                car.Id = dr.GetInt32(0);
+                car.Brand = dr.GetString(1);
+                car.Type = dr.GetString(2);
+                cars.Add(car);
+            } while (dr.Read());
+
+
+
+            
+
+
+
+            conn.Connection.Close();
+        }
+        static void Main(string[] args)
+        {
+            feladat1();
+            Console.WriteLine(cars.Count);
+            Console.ReadLine();
         }
     }
 }
